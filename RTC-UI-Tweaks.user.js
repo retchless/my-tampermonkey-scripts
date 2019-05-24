303b55abec972e2c46cc465e399feaecc0574c71
retchless <retchless@gmail.com>
2019-05-24 13:23:19 -0400

    Improve style of header links

--- RTC-UI-Tweaks.user.js
+++ RTC-UI-Tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RTC-UI-Tweaks
 // @namespace    RTC-UI-Tweaks
-// @version      0.6
+// @version      0.7
 // @description  Hacks to the RTC Web UI to improve my workflow and stop my eyes from bleeding.
 // @author       Rob Retchless
 // @match        https://bajazz05.canlab.ibm.com:9750/ccm/*
@@ -16,23 +16,22 @@
     ui.startup = function() {
         ui.get("aboveBanner").remove();
         ui.get("title").parentNode.parentNode.style = "display: none";
-        ui._bannerTitle.appendChild(dojo.create("span",{innerHTML: "&nbsp;&nbsp;>&nbsp;&nbsp;"}));
+        ui._bannerTitle.appendChild(dojo.create("span",{innerHTML: "&nbsp;&nbsp;&nbsp;"}));
         var links = dojo.create("span", {style:"color: lightcyan; font-size: 9pt; vertical-align: middle;"});
         links.appendChild(dojo.create("a",{
-            style:"text-decoration: underline",
+            className: "custom-header-button",
             href: 'ibm.biz/onqportal',
             innerHTML: "OnQ Portal"
         }));
-        links.appendChild(dojo.create("span",{innerHTML:"&nbsp;&nbsp;|&nbsp;&nbsp;"}));
         links.appendChild(dojo.create("a",{
-            style:"text-decoration: underline",
+            className: "custom-header-button",
             href: 'http://businessanalytics1.fyre.ibm.com/',
             innerHTML: "BA Billboard"
         }));
         ui._bannerTitle.appendChild(links);
         ui._banner.style.marginTop = "-3px";
         dojo.create("link", {href: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap", rel: "stylesheet" }, document.head);
-        dojo.create("style",{innerHTML: "* { font-family: 'IBM Plex Sans'} .banner-icon { display: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .net-jazz-ajax-PageList a, .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .jazz-app-CustomNavbar a { color: #8f90a2; } .jazz-ui-PageTemplate { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .banner-title { text-shadow: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .left, .jazz-ui-PageTemplate > .banner .left > .right, .jazz-ui-PageTemplate > .banner .left > .right > .middle { height: 32px; background: none } html body .jazz-ui-BannerSearchBox-areaInactive .SearchWrapper { background-color: #4e4e55 !important; border: 1px solid #4e4e55 !important;box-shadow: none !important; } .jazz-ui-BannerSearchBox-areaInactive .inactive input { color: #bebec9 } .jazz-ui-PageTemplate > .banner .home-button, .jazz-ui-PageTemplate > .banner .home-button:hover { background-image: none !important; width: 30px !important; height: 36px; } .jazz-ui-PageTemplate > .banner .home-button .home-menu, .jazz-ui-PageTemplate > .banner .home-button .home-menu:hover { width: 24px; background: center center / 16px 12px url(https://www.nicepng.com/png/full/41-415236_menu-menubuttom-hamburger-menu-icon-white.png) no-repeat; }"}, document.head);
+        dojo.create("style",{innerHTML: "* { font-family: 'IBM Plex Sans'} .custom-header-button { background-color: #4e4e55; padding: 3px 8px; border-radius: 4px; margin-right: 8px; } .custom-header-button:hover { background-color: #64646d; text-decoration: none !important; } .banner-icon { display: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .net-jazz-ajax-PageList a, .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .jazz-app-CustomNavbar a { color: #8f90a2; } .jazz-ui-PageTemplate { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .banner-title { text-shadow: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .left, .jazz-ui-PageTemplate > .banner .left > .right, .jazz-ui-PageTemplate > .banner .left > .right > .middle { height: 32px; background: none } html body .jazz-ui-BannerSearchBox-areaInactive .SearchWrapper { background-color: #4e4e55 !important; border: 1px solid #4e4e55 !important;box-shadow: none !important; } .jazz-ui-BannerSearchBox-areaInactive .inactive input { color: #bebec9 } .jazz-ui-PageTemplate > .banner .home-button, .jazz-ui-PageTemplate > .banner .home-button:hover { background-image: none !important; width: 30px !important; height: 36px; } .jazz-ui-PageTemplate > .banner .home-button .home-menu, .jazz-ui-PageTemplate > .banner .home-button .home-menu:hover { width: 24px; background: center center / 16px 12px url(https://www.nicepng.com/png/full/41-415236_menu-menubuttom-hamburger-menu-icon-white.png) no-repeat; }"}, document.head);
         uiStartup();
     };
 })();
