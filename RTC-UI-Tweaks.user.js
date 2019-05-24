392ddff4c7caacbdde395eca8222336f9731746e
retchless <retchless@gmail.com>
2019-05-24 13:13:53 -0400

    Update links

--- RTC-UI-Tweaks.user.js
+++ RTC-UI-Tweaks.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         RTC-UI-Tweaks
 // @namespace    RTC-UI-Tweaks
-// @version      0.5
-// @description  My personal hacks to the RTC Web UI to improve my workflow.
+// @version      0.6
+// @description  Hacks to the RTC Web UI to improve my workflow and stop my eyes from bleeding.
 // @author       Rob Retchless
 // @match        https://bajazz05.canlab.ibm.com:9750/ccm/*
 // @grant        none
@@ -20,14 +20,14 @@
         var links = dojo.create("span", {style:"color: lightcyan; font-size: 9pt; vertical-align: middle;"});
         links.appendChild(dojo.create("a",{
             style:"text-decoration: underline",
-            href: 'https://bajazz05.canlab.ibm.com:9750/ccm/web/projects/Business%20Intelligence#action=com.ibm.team.workitem.runSavedQuery&id=_V-cHAK8SEeebeO4pd0l5xg',
-            innerHTML: "Toolkit Open Defects"
+            href: 'ibm.biz/onqportal',
+            innerHTML: "OnQ Portal"
         }));
         links.appendChild(dojo.create("span",{innerHTML:"&nbsp;&nbsp;|&nbsp;&nbsp;"}));
         links.appendChild(dojo.create("a",{
             style:"text-decoration: underline",
-            href: 'https://bajazz05.canlab.ibm.com:9750/ccm/web/projects/Business%20Intelligence#action=com.ibm.team.dashboard.viewDashboard&team=Cognos%20Analytics/Common%20Components',
-            innerHTML: "Toolkit Dashboard"
+            href: 'http://businessanalytics1.fyre.ibm.com/',
+            innerHTML: "BA Billboard"
         }));
         ui._bannerTitle.appendChild(links);
         ui._banner.style.marginTop = "-3px";
