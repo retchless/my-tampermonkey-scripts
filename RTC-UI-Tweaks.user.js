bfcc4b6967f70234ec99b2de64a2f33d1cf8d93e
retchless <retchless@gmail.com>
2019-05-30 11:24:03 -0400

    Externalize CSS

--- RTC-UI-Tweaks.user.js
+++ RTC-UI-Tweaks.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name         RTC-UI-Tweaks
 // @namespace    RTC-UI-Tweaks
-// @version      0.10
+// @version      0.11
 // @description  Hacks to the RTC Web UI to improve my workflow and stop my eyes from bleeding.
 // @author       Rob Retchless
 // @match        https://bajazz05.canlab.ibm.com:9750/*
-// @grant        none
+// @resource     customCSS RTC-UI-Tweaks.user.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // @downloadURL  https://github.com/retchless/my-tampermonkey-scripts/raw/master/RTC-UI-Tweaks.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
+    GM_addStyle(GM_getResourceText ("customCSS"));
+    dojo.create("link", {href: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap", rel: "stylesheet" }, document.head);
     if (jazz && jazz.app) {
-        var uiStartup = dojo.hitch(jazz.app.currentApplication.ui, jazz.app.currentApplication.ui.startup);
         var ui = jazz.app.currentApplication.ui;
+        var uiStartup = dojo.hitch(ui, ui.startup);
         ui.startup = function() {
             ui.get("aboveBanner").remove();
             ui.get("title").parentNode.parentNode.style = "display: none";
             ui._bannerTitle.appendChild(dojo.create("span",{innerHTML: "&nbsp;&nbsp;&nbsp;"}));
-            var links = dojo.create("span", {style:"color: lightcyan; font-size: 9pt; vertical-align: middle;"});
+            var links = dojo.create("span", {className: "custom-header-buttons"});
             links.appendChild(dojo.create("a",{
                 className: "custom-header-button",
                 href: 'https://ibm.biz/onqportal',
@@ -31,10 +35,8 @@
                 innerHTML: "BA Billboard",
                 target: "billboard"
             }));
-            ui._bannerTitle.appendChild(links);
+            ui._bannerTitle.parentNode.insertBefore(links,ui._bannerLeft);
             ui._banner.style.marginTop = "-3px";
-            dojo.create("link", {href: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap", rel: "stylesheet" }, document.head);
-            dojo.create("style",{innerHTML: "* { font-family: 'IBM Plex Sans'} .custom-header-button { background-color: #4e4e55; padding: 3px 8px; border-radius: 4px; margin-right: 8px; } .custom-header-button:hover { background-color: #64646d; text-decoration: none !important; } .banner-icon { display: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .net-jazz-ajax-PageList a, .jazz-ui-PageTemplate > .navbar-wrapper > .navbar .jazz-app-CustomNavbar a { color: #8f90a2; } .jazz-ui-PageTemplate { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .banner-title { text-shadow: none } .jazz-ui-PageTemplate > .navbar-wrapper > .navbar { background-color: #2a2a2e } .jazz-ui-PageTemplate > .banner .left, .jazz-ui-PageTemplate > .banner .left > .right, .jazz-ui-PageTemplate > .banner .left > .right > .middle { height: 32px; background: none } html body .jazz-ui-BannerSearchBox-areaInactive .SearchWrapper { background-color: #4e4e55 !important; border: 1px solid #4e4e55 !important;box-shadow: none !important; } .jazz-ui-BannerSearchBox-areaInactive .inactive input { color: #bebec9 } .jazz-ui-PageTemplate > .banner .home-button, .jazz-ui-PageTemplate > .banner .home-button:hover { background-image: none !important; width: 30px !important; height: 36px; } .jazz-ui-PageTemplate > .banner .home-button .home-menu, .jazz-ui-PageTemplate > .banner .home-button .home-menu:hover { width: 24px; background: center center / 16px 12px url(https://www.nicepng.com/png/full/41-415236_menu-menubuttom-hamburger-menu-icon-white.png) no-repeat; }"}, document.head);
             uiStartup();
         };
     }
